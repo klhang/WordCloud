0043d2c82882d5f009e1b09ce3fa017120d6ee6d
Klhang <chrisjunhang@gmail.com>
2018-08-25 20:11:46 -0700

    impleatment textarea component

--- src/WordCloud/WordCloud.js
+++ src/WordCloud/WordCloud.js
@@ -1,13 +1,50 @@
 import React from 'react';
 
-
 class WordCloud extends React.Component {
-  constructor(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      texts: {
+        text: "",
+        // other features state,
+        // other features state,
+      }
+    };
 
+    this.handleTextsSubmit = this.handleTextsSubmit.bind(this);
+  }
+
+  handleTextsSubmit(e) {
+    e.preventDefault();
+    this.props.createTextsCloud(this.state.texts).then(() => {
+      let newState = merge({}, this.state, { texts: { text: "" } });
+      this.setState(newState);
+    });
+  }
+
+  updateTextsField() {
+    return e => {
+      let newState = merge({}, this.state, {
+        texts: { text: e.target.value }
+      });
+      this.setState(newState);
+    };
   }
 
   render(){
-
+    return (
+      <div>
+        <br></br>
+          <textarea
+            placeholder="Please paste your text here"
+            onChange={this.updateTextsField()}
+            value={this.state.texts.text}
+          />
+        <button onClick={this.handleTextsSubmit}>
+          <span>Submit</span>
+        </button>
+      </div>
+     );
   }
 
 export default WordCloud;
