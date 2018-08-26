a7b1710b1db86518d27bbef0972411c5058ca2be
Klhang <chrisjunhang@gmail.com>
2018-08-26 10:39:17 -0700

    test and refactor

--- src/WordCloud/WordCloud.js
+++ src/WordCloud/WordCloud.js
@@ -1,89 +1,97 @@
 import React from 'react';
 import merge from "lodash/merge";
+//import { render } from 'react-dom';
+import TagCloud from 'react-d3-cloud';
 
 class WordCloud extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      texts: {
-        text: "",
-        // other features state,
-        // other features state,
-        data: []
-      }
+      texts: "",
+      data: [],
+      frontSize: 40
+      //other features
     };
-
     this.handleTextsSubmit = this.handleTextsSubmit.bind(this);
   }
 
   handleTextsSubmit(e) {
     e.preventDefault();
-    let text = this.cleanTexts(this.state.texts.text)
-    // console.log(text)
-    this.generateCloud(text).then(() => {
-        let newState = merge({}, this.state, { texts: { text: "" } });
-        this.setState(newState);
-    });
-  }
 
-  generateCloud(str){
-    let map = buildMap(str);
-    let tags = [];
-    Object.keys(map).forEach(key => {
-      let tag = {"text"  : key,
-                 "value" : map[key]};
-      tags.add(tag);
-    });
-    let newState = merge({}, this.state, {texts: { data: tags}});
-  };
+    let texts = this.cleanTexts(this.state.texts);
+    let tags = this.generateTags(texts);
 
-  buildHash(str){
-    let arr = str.split(" ");
-    let map = {};
-    for (let i = 0; i < arr.length; i++){
-      let word = arr[i];
-      if (words in map){
-        map[words] = map[words] + 1;
-      } else {
-        map[words] = 1;
-      }
-    }
-    return map;
-  }
-
-  updateTextsField() {
-    return e => {
-      let newState = merge({}, this.state, {
-        texts: { text: e.target.value }
-      });
-      this.setState(newState);
-    };
+    let newState = merge({}, this.state, { texts: "" , data: tags });
+    this.setState(newState);
   }
 
   cleanTexts(str) {
-  	if (typeof str !== 'string') {
-  		throw new TypeError('Expected a string');
-  	}
-  	return str.replace(/[&\/\\#,+\(\)$~%\.!^'"\;:*?\[\]<>{}]/g, '');
+    if (typeof str !== 'string') {
+      throw new TypeError('Expected a string');
+    }
+    return str.replace(/[&\/\\#,+\(\)$~%\.!^'"\;:*?\[\]<>{}]/g, '');
 
     // var s = "This., -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation";
     // var punctuationless = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     // var finalString = punctuationless.replace(/\s{2,}/g," ");
   };
 
+  generateTags(str){
+    let frequencyMap = this.buildFrequencyMap(str);
+    let tags = [];
 
+    Object.keys(frequencyMap).forEach(key => {
+      let tag = {"text"  : key,
+                 "value" : frequencyMap[key]};
+      tags.push(tag);
+    });
+
+    return tags;
+  };
+
+  buildFrequencyMap(str){
+    let arr = str.split(" ");
+    let map = {};
+
+    for (let i = 0; i < arr.length; i++){
+      let word = arr[i];
+      if (word in map){
+        map[word] = map[word] + 1;
+      } else {
+        map[word] = 1;
+      }
+    }
+
+    return map;
+  }
+
+  updateTextsField() {
+    return e => {
+      let newState = merge({}, this.state, { texts: e.target.value });
+      this.setState(newState);
+    };
+  }
+
+  // test(){
+  //   let newState = merge({}, this.state, {data: [{"text": "qqqqqqqqqq", "value": 50}, {"text": "BBBBBBBB", "value": 50}]});
+  //   this.setState(newState);
+  //   console.log(this.state.texts.data);
+  // }
 
   render(){
     return (
       <div>
-        <TagCloud
-          data={data}
-        />
+        <div>
+          <TagCloud
+            data={this.state.data}
+            fontSizeMapper={this.state.fontSize}
+          />
+        </div>
         <br></br>
           <textarea
             placeholder="Please paste your text here"
             onChange={this.updateTextsField()}
-            value={this.state.texts.text}
+            value={this.state.texts}
           />
         <button onClick={this.handleTextsSubmit}>
           <span>Submit</span>
